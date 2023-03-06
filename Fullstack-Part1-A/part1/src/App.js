594692daf03ceb15de4c7755519bc4e7308e8723
Clay <clayhop901@gmail.com>
2023-03-06 10:43:09 -0500

    Exercise 1.1 'course information, step1'

---
+++ Fullstack-Part1-A/part1/src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.sumofex}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercise1 = 10
+  const part2 = 'Using props to pass data'
+  const exercise2 = 7
+  const part3 = 'State of a component'
+  const exercise3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercise={exercise1} />
+      <Content part={part2} exercise={exercise2} />
+      <Content part={part3} exercise={exercise3} />
+      <Total sumofex={exercise1 + exercise2 + exercise3} />
+    </div>
+  )
+}
+
+export default App;

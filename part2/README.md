2d03eeed440cde557949fedc28bea62485029c78
CHH02 <clayhop901@gmail.com>
2025-05-06 15:35:06 -0500

    Exercise 2.1 'Course information step 6'
    This commit starts off Part 2's exercises and this specific commit is the completion of exercise 2.1.

---
+++ part2/README.md
@@ -0,0 +1,11 @@
+# FullStack-Part2
+This is for the submission of exercises 2.1-2.20 of the Full OpenStack course. See Full Stack open part 2 [here](https://fullstackopen.com/en/part2)
+
+## Objective
+- Ex 2.5
+    - ex 2.1-2.5 are exercises that implement better practices on ex 1.5's application, e.g., using higher-order functions and modularity for optimization.
+
+## My Apps
+
+### Ex 2.1
+![PNG of CHH02's Exercise 2.1 submission](./public/Ex2-1_Screenshot.png)
---
+++ part2/src/App.jsx
@@ -0,0 +1,73 @@
+/* FullStackOpen Exercise 2.1 */
+
+// Header component for displaying the title of a course
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
+
+// Part component for displaying the indvidual parts of a course's content
+const Part = (props) => {
+  console.log(props)
+  return <p>{props.part.name} {props.part.exercises}</p>
+}
+
+// Content component for displaying the content of a course
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+// Total component for calculating a sum of exercises displaying in our App component seen further below
+const Total = (props) => {
+  console.log(props)
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+}
+
+// Course component for dispalying a course in the App component seen further below
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+
+      {/* Below is the sum of exercises which is commented out because we don't need the sum to complete exercise 2.1 */}
+      {/* <Total parts={course.parts} /> */}
+    </div>
+  )
+}
+
+// App component for displaying FullStackOpen's course information
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
